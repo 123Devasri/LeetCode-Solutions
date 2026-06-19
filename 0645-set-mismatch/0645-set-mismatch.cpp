37c5ef447401a81e4fd57b865188af92c84463f5
Deva Sri <rdevasri123@gmail.com>
2026-06-19 22:59:39 +0530

    Time: 6 ms (37.6%), Space: 25.2 MB (76.52%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int dup=-1,miss=-1,i;
+        for(i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
+                dup=nums[i];
+            }
+            else if (nums[i]>nums[i-1]+1){
+                miss=nums[i-1]+1;
+            }
+        }
+        if(nums[0]!=1){
+            miss=1;
+        }
+        if(nums.back()!=nums.size()){
+            miss=nums.size();
+        }
+        return {dup,miss};
+    }
+};
