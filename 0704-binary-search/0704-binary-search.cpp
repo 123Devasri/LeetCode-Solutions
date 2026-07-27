c73ebab3143d99a05f72d7cdef8861594fc3289e
Deva Sri <rdevasri123@gmail.com>
2026-07-27 22:49:36 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.95%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int right=nums.size()-1,left=0,mid=0;
+        while(right>=left){
+          mid=left+(right-left)/2;
+          if(nums[mid]==target)return mid;
+          else if(nums[mid]>target)right=mid-1;
+          else left=mid+1;
+        }
+        return -1;
+        }
+};
