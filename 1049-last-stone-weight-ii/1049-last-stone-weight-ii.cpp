68e43a7f4eb2a3adc616f47315137f218dd80efe
Deva Sri <rdevasri123@gmail.com>
2026-09-23 23:51:42 +0530

    Time: 0 ms (100%), Space: 10.6 MB (79.31%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int total = 0;
+
+        for(int x : stones)
+            total += x;
+
+        int target = total / 2;
+
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for(int x : stones) {
+            for(int j = target; j >= x; j--) {
+                dp[j] = dp[j] || dp[j - x];
+            }
+        }
+
+        for(int j = target; j >= 0; j--) {
+            if(dp[j]) {
+                return total - 2 * j;
+            }
+        }
+
+        return 0;
+    }
+};
